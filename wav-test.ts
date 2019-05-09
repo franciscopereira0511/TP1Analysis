94e4b6a42c087ac80b438628de662167dcdec1bb
Francisco Pereira <franciscopereira0511@gmail.com>
2019-05-09 15:00:43 -0600

    Update wav-test.ts

--- wav-test.ts
+++ wav-test.ts
@@ -18,7 +18,6 @@
 //Encontrar un segundo que haga match con un segundo de S1
 //Fijar un porcentaje de aceptación al comparar. Definir si es 80% igual. 
 
-
 import * as fs from 'fs';
 // import { complex as fft } from 'fft';
 import * as WavEncoder from 'wav-encoder';
@@ -37,6 +36,7 @@ const readFile = (filepath: string) => {
 };
 
 
+
 //Para hallar el valor máximo y mínimo del array de puntos
 function findMinMax(array:number[]) {
   let min = array[0], max = array[0];
@@ -50,31 +50,78 @@ function findMinMax(array:number[]) {
   return [min, max];
 }
 
+function secondWithMostPeaks(peaks:number[][]){
+  var result:number = 0;
+  for(var i=0;i<peaks.length;i++){
+    if(peaks[i][0]>result){
+      result=peaks[i][0];
+    }
+  }
+  return result;
+}
+
 //Para hallar la cantidad de picos por segundo, siendo picos los puntos que están por encima del 60% del valor máximo y mínimo
 function getPeaks(array:number[]){
-  //console.log("Largo del array: " + array.length);
 
   let positivePeakCondition:number = findMinMax(array)[1]*0.60;
   let negativePeakCondition:number = findMinMax(array)[0]*0.60;
-  //console.log("positivePeakCondition: "+ positivePeakCondition + " negativePeakCondition: " + negativePeakCondition);
-
-  let result:number[] = [];
+  let result:any[][] = [];
   let peakCounter:number = 0;
+  let firstPeak:number = 0;
 
   //Recorre los puntos del array para hallar picos
   for(let i=0;i<array.length;i++){
     //Divide los picos por segundo
     for(let j=0;j<44100;j++){
+      if(j==0){
+        firstPeak = i;
+      }
       if(array[i]<negativePeakCondition || array[i]>positivePeakCondition){
         peakCounter += 1;
       }
       i++;
     }
     //Cada 44100 elementos (1 segundo) hace un push en el resultado para incluir la cantidad de picos en ese segundo
-    result.push(peakCounter);
+    result.push([peakCounter,firstPeak]);
     peakCounter=0;
+    firstPeak=0;
   }
   return result;
+}
+
+
+function statisticsTable(peaks:any[][],rows:number){
+  var result = Array(rows+1).fill([]);
+  var mostPeaks = secondWithMostPeaks(peaks)
+  var rowRange = Math.floor(mostPeaks/rows);
+  console.log(rowRange);
+  var resultIndex = 0;
+
+  //Recorre cada segundo de la lista de picos. En cada if se le suma a la fila de la tabla (dependiendo de la cantidad de picos del segundo) 
+  //En indice 0: la cantidad de picos, Indice 1: se agregan los indices de los picos que estaban en el segundo
+  for(var i=0; i<peaks.length; i++){
+    for(let minCondition=0; minCondition<mostPeaks; minCondition+=rowRange){
+      if(minCondition <= peaks[i][0] && peaks[i][0] <= minCondition+rowRange){
+        result[resultIndex].push(peaks[i]);
+        break;
+      }
+      else{
+        resultIndex+=1;
+      }
+      
+    }
+    console.log(result[0].length,result[1].length,result[2].length,result[3].length,result[4].length,result[5].length);
+    console.log("#####################");
+    resultIndex=0;
+  }
+
+  //console.log(result[0]);
+  //console.log(result[1]);
+  //console.log(result[2]);
+  //console.log(result[3]);
+  console.log(result[4]);
+  //console.log(result[5]);
+
 }
 
 //Estadistica: Reducir las comparaciones a hacer para buscar match haciendo que 
@@ -133,7 +180,8 @@ function generarRelieves(s1:number[]){
     resultTemp.push(s1[i]);
 
     //Si pasa de positivo a negativo, se terminó la forma
-    if(Math.sign(s1[i])==1 && Math.sign(s1[i+1])==-1 && resultTemp.length>50){
+    if(Math.sign(s1[i])==1 && Math.sign(s1[i+1])==-1 && resultTemp.length>100){
+      //Aqui debo llamar a la funcion que identifica la forma de resultTemp
       result.push([resultTemp[0],"Forma",resultTemp.length]);
     }
     //Si pasa de negativo a positivo, inicia la forma
@@ -141,16 +189,23 @@ function generarRelieves(s1:number[]){
       resultTemp=[];
       resultTemp.push(i);
     }
-
-
-    
   }
-
   console.log(result);
   return result;
 }
 
-readFile("C:\\Users\\User\\Desktop\\Clases 5to Semestre\\Análisis de Algoritmos\\alg2019-master\\s1.wav").then((buffer) => {
+
+//Define la forma segun el porcentaje de puntos que hay en cada altura. Puedo encontrar el punto más alto y comparar con los de los lados. 
+//Si hay mucha diferencia, es un pico, si no, es una meseta. Si el punto más alto está por debajo de 0.10, es un silencio. 
+function definirRelieve(resultTemp:number[]){
+  var max:number = findMinMax(resultTemp)[1];
+  //if(max<0.)
+}
+
+
+
+
+readFile("C:\\Users\\User\\Desktop\\Clases 5to Semestre\\Análisis de Algoritmos\\alg2019-master\\ChopSuey.wav").then((buffer) => {
   return WavDecoder.decode(buffer);
 }).then(function(audioDataS1) {
   console.log("ampliando 30%");
@@ -164,7 +219,16 @@ readFile("C:\\Users\\User\\Desktop\\Clases 5to Semestre\\Análisis de Algoritmos
     audioDataS1.channelData[0][i+44100*6] = audioDataS1.channelData[0][i];
   }
 
-  readFile("C:\\Users\\User\\Desktop\\Clases 5to Semestre\\Análisis de Algoritmos\\alg2019-master\\s2.wav").then((buffer) => {
+  function generateWAV(leftChannel:number[],rightChannel:number[]){
+    audioDataS1.channelData[0] = new Float32Array(leftChannel);
+    audioDataS1.channelData[1] = new Float32Array(rightChannel);
+    console.log("writing...");
+    WavEncoder.encode(audioDataS1).then((buffer: any) => {
+      fs.writeFileSync("./Sound/WaV.wav", new Buffer(buffer));
+    });
+  }
+
+  readFile("C:\\Users\\User\\Desktop\\Clases 5to Semestre\\Análisis de Algoritmos\\alg2019-master\\ChopSueySample.wav").then((buffer) => {
     return WavDecoder.decode(buffer);
   }).then(function(audioDataS2) {
     //console.log("ampliando 30%");
@@ -177,6 +241,8 @@ readFile("C:\\Users\\User\\Desktop\\Clases 5to Semestre\\Análisis de Algoritmos
     for(var i=44100*11; i<44100*16; i++) {
       audioDataS2.channelData[0][i+44100*6] = audioDataS2.channelData[0][i];
     }
+
+    
 
 
   let valuesS1 = audioDataS1.channelData[0];
@@ -191,8 +257,9 @@ readFile("C:\\Users\\User\\Desktop\\Clases 5to Semestre\\Análisis de Algoritmos
   //console.log(peaksS2);
   //console.log("Matches: ");
   //console.log(match(prepareComparison(peaksS1,peaksS2),peaksS2,100));
-
-  generarRelieves(valuesS1);
+  //generarRelieves(valuesS1);
+  statisticsTable(peaksS1,5);
+  //generateWAV(audioDataS1.channelData[0],audioDataS1.channelData[1])
 
 });
 });
